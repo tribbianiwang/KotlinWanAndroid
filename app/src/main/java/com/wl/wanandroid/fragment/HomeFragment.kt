cc0569abebbc57c1cbe650c961ffbd05ee2696db
wanglei <1130455004@qq.com>
2020-02-10 15:58:58 +0800

    首页的公众号相关内容加载显示正常，一排显示五个平铺在界面上

--- app/src/main/java/com/wl/wanandroid/fragment/HomeFragment.kt
+++ app/src/main/java/com/wl/wanandroid/fragment/HomeFragment.kt
@@ -6,20 +6,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
 import com.tmall.ultraviewpager.UltraViewPager
 import com.wl.wanandroid.R
 import com.wl.wanandroid.adapter.PagerHomeBannerAdapter
+import com.wl.wanandroid.adapter.RvHomePublicNumberAdapter
 import com.wl.wanandroid.bean.BannerBean
-import com.wl.wanandroid.bean.Data
+import com.wl.wanandroid.bean.PublicNumberListBean
 import com.wl.wanandroid.utils.AppConstants.HOME_BANNER_LOOP_TIME
+import com.wl.wanandroid.utils.LogUtils
 import com.wl.wanandroid.viewmodel.BannerViewModel
+import com.wl.wanandroid.viewmodel.PublicNumberListViewModel
 import kotlinx.android.synthetic.main.layout_fragment_home.*
 
 
 class HomeFragment : BaseFragment() {
 
     lateinit var bannerViewModel : BannerViewModel
-    var bannerDatas:List<Data>?=null
+    lateinit var publicNumberListModel:PublicNumberListViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,9 +36,15 @@ class HomeFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+        rv_publicnumber.layoutManager = GridLayoutManager(context,2,GridLayoutManager.HORIZONTAL,false)
+
         bannerViewModel = ViewModelProviders.of(this).get(BannerViewModel::class.java)
+        publicNumberListModel = ViewModelProviders.of(this).get(PublicNumberListViewModel::class.java)
 
         lifecycle.addObserver(bannerViewModel)
+        lifecycle.addObserver(publicNumberListModel)
 
         var bannerBeanObserver = Observer<BannerBean>{
             ulBanner?.setScrollMode(UltraViewPager.ScrollMode.HORIZONTAL)
@@ -50,12 +60,24 @@ class HomeFragment : BaseFragment() {
 
         }
 
+        var publicNumberObserver = Observer<PublicNumberListBean>{
+            var rvHomePublicNumberAdapter = RvHomePublicNumberAdapter(it.data)
+            rv_publicnumber.adapter = rvHomePublicNumberAdapter
+
+        }
+
 
         bannerViewModel.baseResultLiveData.observe( this,bannerBeanObserver)
         bannerViewModel.queryStatusLiveData.observe(this,queryStatusObserver)
         bannerViewModel.errorMsgLiveData.observe(this,errorMsgObserver)
+        publicNumberListModel.baseResultLiveData.observe(this,publicNumberObserver)
+        publicNumberListModel.queryStatusLiveData.observe(this,queryStatusObserver)
+        publicNumberListModel.errorMsgLiveData.observe(this,errorMsgObserver)
+
+
 
         bannerViewModel.getBannerData()
+        publicNumberListModel.getPublicNumberList()
     }
 
 
